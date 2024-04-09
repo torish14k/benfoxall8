64d5c45b044591a6d9422cc2961dcb3fb8a69ee7
torish14k <uanganxinyiw829@163.com>
2024-04-10 03:59:56 +0800

    jiyong@huawei.com
    Signed-off-by: jiyong <jiyong@huawei.com>

--- ace/ace_standard/project/entry/src/main/js/default/pages/routerPush/index.js
+++ ace/ace_standard/project/entry/src/main/js/default/pages/routerPush/index.js
@@ -12,32 +12,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import file from '@system.file'
-import {Core, ExpectExtend, ReportExtend} from 'deccjsunit/index'
-
 export default {
     data: {
-        title: ""
+        title: "router push test"
     },
     onInit() {
-        this.title = this.$t('strings.world');
-        const core = Core.getInstance()
-        const expectExtend = new ExpectExtend({
-            'id': 'extend'
-        })
-        const reportExtend = new ReportExtend(file)
-        core.addService('expect', expectExtend)
-        core.addService('report', reportExtend)
-        core.init()
-
-        const configService = core.getDefaultService('config')
-        configService.setConfig(this)
-
-        require('../../../test/List.test')
-        core.execute()
     },
     onShow() {
-        console.info('onShow finish')
     },
     onReady() {
     },
