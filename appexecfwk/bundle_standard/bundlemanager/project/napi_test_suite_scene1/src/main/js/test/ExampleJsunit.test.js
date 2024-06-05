87f05048d6bac2f5b4c6c955959eea3e6e1d080a
torish14k <uanganxinyiw829@163.com>
2024-06-06 03:12:56 +0800

    Modify test case
    Signed-off-by: chensi10 <chen.s@neusoft.com>

--- appexecfwk/bundle_standard/bundlemanager/project/napi_test_suite_scene1/src/main/js/test/ExampleJsunit.test.js
+++ appexecfwk/bundle_standard/bundlemanager/project/napi_test_suite_scene1/src/main/js/test/ExampleJsunit.test.js
@@ -124,4 +124,325 @@ describe('appInfoTest', function () {
         done()
     })
 
+    it('getBundleInfo_0100', 0, async function (done) {
+        var datainfo = await demo.getBundleInfo('com.example.myapplication1',1)
+        expect(datainfo.name.length).assertLarger(0)
+        expect(datainfo.vendor.length).assertLarger(0)
+        expect(datainfo.versionCode).assertEqual(1)
+        expect(datainfo.versionName).assertEqual(1.0)
+        expect(datainfo.appInfo.name.length).assertLarger(0)
+        expect(datainfo.appInfo.description.length).assertLarger(0)
+        expect(datainfo.appInfo.label.length).assertLarger(0)
+        expect(datainfo.appInfo.icon.length).assertLarger(0)
+        expect(datainfo.appInfo.descriptionId).assertEqual(16777217)
+        done()
+    })
+
+    it('getBundleInfo_0400', 0, async function (done) {
+        var datainfo = await demo.getBundleInfo('error',1);
+        expect(datainfo.name.length).assertEqual(0)
+        done();
+    })
+
+    it('getBundleInfo_0500', 0, async function (done) {
+        var datainfo = await demo.getBundleInfo();
+        expect(datainfo.name.length).assertEqual(0)
+        done();
+    })
+
+    it('getBundleInfo_0600', 0, async function (done) {
+        await demo.getBundleInfo('com.example.myapplication1',1,OnReceiveEvent)
+        function OnReceiveEvent(err,datainfo) {
+            expect(datainfo.name.length).assertLarger(0)
+            expect(datainfo.vendor.length).assertLarger(0)
+            expect(datainfo.versionCode).assertEqual(1)
+            expect(datainfo.versionName).assertEqual(1.0)
+            expect(datainfo.appInfo.name.length).assertLarger(0)
+            expect(datainfo.appInfo.description.length).assertLarger(0)
+            expect(datainfo.appInfo.label.length).assertLarger(0)
+            expect(datainfo.appInfo.icon.length).assertLarger(0)
+            expect(datainfo.appInfo.descriptionId).assertEqual(16777217)
+        }
+        done()
+    })
+
+    it('getBundleInfo_0900', 0, async function (done) {
+        await demo.getBundleInfo('error',1,OnReceiveEvent)
+        function OnReceiveEvent(err, datainfo) {
+            expect(datainfo.name.length).assertEqual(0)
+        }
+        done()
+    })
+
+    it('getBundleInfo_1000', 0, async function (done) {
+        await demo.getBundleInfo(OnReceiveEvent)
+        function OnReceiveEvent(err, datainfo) {
+            expect(datainfo.name.length).assertEqual(0)
+        }
+        done()
+    })
+
+    it('getBundleArchiveInfo_0100',  0, async function (done) {
+        var datainfo = await demo.getBundleArchiveInfo('/data/bmsJstest1.hap',1)
+        expect(datainfo.name.length).assertLarger(0)
+        expect(datainfo.vendor.length).assertLarger(0)
+        expect(datainfo.versionCode).assertEqual(1)
+        expect(datainfo.versionName).assertEqual(1.0)
+        expect(datainfo.appInfo.name.length).assertLarger(0)
+        expect(datainfo.appInfo.description.length).assertLarger(0)
+        expect(datainfo.appInfo.label.length).assertLarger(0)
+        expect(datainfo.appInfo.icon.length).assertLarger(0)
+        done()
+    })
+
+    it('getBundleArchiveInfo_0400',  0, async function (done) {
+        var datainfo = await demo.getBundleArchiveInfo('/data/error.hap',1)
+        expect(datainfo.name.length).assertEqual(0)
+        done()
+    })
+
+    it('getBundleArchiveInfo_0500',  0, async function (done) {
+        var datainfo = await demo.getBundleArchiveInfo(' ',1)
+        expect(datainfo.name.length).assertEqual(0)
+        done()
+    })
+
+    it('getBundleArchiveInfo_0600',  0, async function (done) {
+        await demo.getBundleArchiveInfo('/data/bmsJstest1.hap',1,OnReceiveEvent)
+        function OnReceiveEvent(err, datainfo) {
+            expect(datainfo.name.length).assertLarger(0)
+            expect(datainfo.vendor.length).assertLarger(0)
+            expect(datainfo.versionCode).assertEqual(1)
+            expect(datainfo.versionName).assertEqual(1.0)
+            expect(datainfo.appInfo.name.length).assertLarger(0)
+            expect(datainfo.appInfo.description.length).assertLarger(0)
+            expect(datainfo.appInfo.label.length).assertLarger(0)
+            expect(datainfo.appInfo.icon.length).assertLarger(0)
+        }
+        done()
+    })
+
+    it('getBundleArchiveInfo_0900',  0, async function (done) {
+        await demo.getBundleArchiveInfo('/data/error.', 1,OnReceiveEvent)
+        function OnReceiveEvent(err, datainfo) {
+            expect(datainfo.name.length).assertEqual(0)
+        }
+        done()
+    })
+
+    it('getBundleArchiveInfo_1000',  0, async function (done) {
+        await demo.getBundleArchiveInfo(' ', 1,OnReceiveEvent)
+        function OnReceiveEvent(err, datainfo) {
+            expect(datainfo.name.length).assertEqual(0)
+        }
+        done()
+    })
+
+    it('queryAbilityByWant_0100', 0, async function (done) {
+        var datainfo =
+            await demo.queryAbilityByWant({
+                want:{
+                    action: "action.system.home",
+                    entities:["entity.system.home"],
+                    elementName: {
+                        deviceId: "0",
+                        bundleName : "com.example.myapplication1",
+                        abilityName : ".MainAbility",
+                    },}},{
+                params:{
+                    flags: 8 ,
+                    userId:"0" ,
+                }
+            })
+        expect(datainfo.name.length).assertLarger(0)
+        expect(datainfo.label.length).assertLarger(0)
+        expect(datainfo.description.length).assertLarger(0)
+        expect(datainfo.icon.length).assertLarger(0)
+        expect(datainfo.moduleName.length).assertLarger(0)
+        expect(datainfo.bundleName.length).assertLarger(0)
+        expect(datainfo.applicationInfo.name.length).assertLarger(0)
+        expect(datainfo.applicationInfo.description.length).assertLarger(0)
+        expect(datainfo.applicationInfo.icon.length).assertLarger(0)
+        expect(datainfo.applicationInfo.label.length).assertLarger(0)
+        done();
+    })
+
+    it('queryAbilityByWant_0400', 0, async function (done) {
+        var datainfo =
+            await demo.queryAbilityByWant({
+                want:{
+                    action: "action.system.home",
+                    entities:["entity.system.home"],
+                    elementName: {
+                        deviceId : "0",
+                        bundleName : "com.example.myapplication1",
+                        abilityName : "",
+                    },}},{
+                params:{
+                    flags: 8 ,
+                    userId:"0" ,
+                }
+            })
+        expect(datainfo.name.length).assertEqual(0)
+        done();
+    })
+
+    it('queryAbilityByWant_0500', 0, async function (done) {
+        var datainfo =
+            await demo.queryAbilityByWant({
+                want:{
+                    action: "action.system.home",
+                    entities:["entity.system.home"],
+                    elementName: {
+                        deviceId : "0",
+                        bundleName : "",
+                        abilityName : "",
+                    },}},{
+                params:{
+                    flags: 8 ,
+                    userId:"0" ,
+                }
+            })
+        expect(datainfo.name.length).assertEqual(0)
+        done();
+    })
+
+    it('queryAbilityByWant_0600', 0, async function (done) {
+        await demo.queryAbilityByWant({
+            want: {
+                action: "action.system.home",
+                entities: ["entity.system.home"],
+                elementName: {
+                    deviceId: "0",
+                    bundleName: "com.example.myapplication1",
+                    abilityName: ".MainAbility",
+                },
+            }
+        }, {
+            params: {
+                flags: 8,
+                userId: "0",
+            }
+        },OnReceiveEvent)
+        function OnReceiveEvent(err,datainfo) {
+            expect(datainfo.name.length).assertLarger(0)
+            expect(datainfo.label.length).assertLarger(0)
+            expect(datainfo.description.length).assertLarger(0)
+            expect(datainfo.icon.length).assertLarger(0)
+            expect(datainfo.moduleName.length).assertLarger(0)
+            expect(datainfo.bundleName.length).assertLarger(0)
+            expect(datainfo.applicationInfo.name.length).assertLarger(0)
+            expect(datainfo.applicationInfo.description.length).assertLarger(0)
+            expect(datainfo.applicationInfo.icon.length).assertLarger(0)
+            expect(datainfo.applicationInfo.label.length).assertLarger(0)
+            done()
+        }
+    })
+
+    it('queryAbilityByWant_0900', 0, async function (done) {
+        await demo.queryAbilityByWant({
+            want: {
+                action: "action.system.home",
+                entities: ["entity.system.home"],
+                elementName: {
+                    deviceId: "0",
+                    bundleName: "com.example.myapplication1",
+                    abilityName: "",
+                },
+            }
+        }, {
+            params: {
+                flags: 8,
+                userId: "0",
+            }
+        },OnReceiveEvent)
+        function OnReceiveEvent(err, datainfo) {
+            expect(datainfo.name.length).assertEqual(0)
+            done();
+        }
+    })
+
+    it('queryAbilityByWant_1000', 0, async function (done) {
+        await demo.queryAbilityByWant({
+            want: {
+                action: "action.system.home",
+                entities: ["entity.system.home"],
+                elementName: {
+                    deviceId: "0",
+                    bundleName: "",
+                    abilityName: "",
+                },
+            }
+        }, {
+            params: {
+                flags: 8,
+                userId: "0",
+            }
+        },OnReceiveEvent)
+        function OnReceiveEvent(err,datainfo) {
+            expect(datainfo.name.length).assertEqual(0)
+            done()
+        }
+    })
+
+    it('getPermissionDef_0100', 0, async function(done){
+        await demo.getPermissionDef('com.permission.CAMERA').then((infos) => {
+            expect(typeof infos).assertEqual("object");
+
+            expect(infos.name).assertEqual("com.permission.CAMERA");
+            expect(infos.grantMode).assertEqual(1);
+            expect(infos.availableScope).assertEqual(2);
+            expect(infos.label).assertEqual("CAMERA permission");
+            expect(infos.description).assertEqual("CAMERA permission in detail");
+        });
+        done();
+    })
+
+    it('getPermissionDef_0600', 0, async function(done){
+        console.info("neu========================getPermissionDef_0100: for begin");
+
+        await demo.getPermissionDef('com.permission.CAMERA', OnReceiveEvent);
+
+        function OnReceiveEvent(err, data) {
+            expect(data.name).assertEqual("com.permission.CAMERA");
+            expect(data.grantMode).assertEqual(1);
+            expect(data.availableScope).assertEqual(2);
+            expect(data.label).assertEqual("CAMERA permission");
+            expect(data.description).assertEqual("CAMERA permission in detail");
+        }
+
+        done();
+
+    })
+    
+    it('getPermissionDef_0400', 0, async function(done){
+        await demo.getPermissionDef('').then((infos) => {
+            expect(typeof infos).assertEqual("object");
+            expect(infos.name).assertEqual("");
+            expect(infos.grantMode).assertEqual("");
+            expect(infos.availableScope).assertEqual(0);
+            expect(infos.label).assertEqual("");
+            expect(infos.description).assertEqual("");
+        });
+        done();
+    })
+    it('getPermissionDef_0900', 0, async function(done)
+    {
+
+        await demo.getPermissionDef('', OnReceiveEvent);
+
+        function OnReceiveEvent(err, data) {
+            expect(typeof data).assertEqual("object");
+
+            expect(data.name).assertEqual("");
+
+            expect(data.availableScope).assertEqual(0);
+            expect(data.grantMode).assertEqual("");
+            expect(data.label).assertEqual("");
+            expect(data.description).assertEqual("");
+        }
+
+        done();
+    })
+
 })
---
+++ appexecfwk/bundle_standard/bundlemanager/project/napi_test_suite_scene4/src/main/js/test/ExampleJsunit.test.js
@@ -0,0 +1,328 @@
+import app from '@system.app'
+import {describe, beforeAll, beforeEach, afterEach, afterAll, it, expect} from 'deccjsunit/index'
+import demo from '@ohos.bundle'
+
+describe('appInfoTest', function () {
+    
+    it('getPermissionDef_0500', 0, async function(done){
+
+        await demo.getPermissionDef('error').then((infos) => {
+
+            expect(typeof infos).assertEqual("object");
+            expect(infos.name).assertEqual("");
+            expect(infos.grantMode).assertEqual("");
+            expect(infos.availableScope).assertEqual(0);
+            expect(infos.label).assertEqual("");
+            expect(infos.description).assertEqual("");
+        });
+
+        done();
+    })
+
+  
+    it('getPermissionDef_1000', 0, async function(done){
+        await demo.getPermissionDef('error', OnReceiveEvent);
+
+        function OnReceiveEvent(err, data) {
+            expect(typeof data).assertEqual("object");
+            expect(data.name).assertEqual("");
+            expect(data.availableScope).assertEqual(0);
+            expect(data.grantMode).assertEqual("");
+            expect(data.label).assertEqual("");
+            3.
+            expect(data.description).assertEqual("");
+        }
+
+        done();
+    })
+    it('install_0100', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.uninstall('com.example.myapplication1', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication2', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication4', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication5', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication6', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.install(['/data/bmsJstest1.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.uninstall('com.example.myapplication1', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("SUCCESS");
+            }
+
+        });
+        done();
+    })
+    it('install_0200', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.install(['/data/bmsJstest2.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.install(['/data/bmsJstest3.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.uninstall('com.example.myapplication2', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("SUCCESS");
+            }
+
+        });
+        done();
+    })
+    it('install_0300', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.install(['/data/bmsJstest4.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.install(['/data/bmsJstest5.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.install(['/data/bmsJstest6.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.uninstall('com.example.myapplication4', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication5', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication6', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("SUCCESS");
+            }
+
+        });
+        done();
+    })
+    it('install_0400', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.install(['error'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("STATUS_INSTALL_FAILURE_INVALID");
+            }
+
+        });
+        done();
+    })
+    it('install_0500', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.install([''], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("STATUS_INSTALL_FAILURE_INVALID");
+            }
+
+        });
+        done();
+    })
+    it('uninstall_0100', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.install(['/data/bmsJstest1.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication1', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("SUCCESS");
+            }
+        });
+        done();
+    })
+    it('uninstall_0200', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.install(['/data/bmsJstest1.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.install(['/data/bmsJstest2.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication1', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.uninstall('com.example.myapplication2', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("SUCCESS");
+            }
+        });
+        done();
+    })
+    it('uninstall_0300', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.install(['/data/bmsJstest4.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.install(['/data/bmsJstest5.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.install(['/data/bmsJstest6.hap'], {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            });
+            data.uninstall('com.example.myapplication4', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.uninstall('com.example.myapplication5', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            data.uninstall('com.example.myapplication6', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("SUCCESS");
+            }
+        });
+        done();
+    })
+
+    it('uninstall_0400', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.uninstall('', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("STATUS_UNINSTALL_FAILURE_ABORTED");
+            }
+        });
+        done();
+    })
+    it('uninstall_0500', 0, async function (done) {
+        await demo.getBundleInstaller().then((data) => {
+            data.uninstall('', {
+                param: {
+                    userId: 0,
+                    isKeepData: false
+                }
+            }, OnReceiveinstallEvent);
+
+            function OnReceiveinstallEvent(err, data) {
+                expect(typeof data).assertEqual("object");
+                expect(data.statusMessage).assertEqual("STATUS_UNINSTALL_FAILURE_ABORTED");
+            }
+        });
+        done();
+    })
+})
