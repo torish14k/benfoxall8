e11f72e38f9be883d5ee442150102911d81d5754
torish14k <uanganxinyiw829@163.com>
2026-01-28 12:23:52 +0800

    add new testcase Signed-off-by: liuxiaopei <liuxiaopei@huawei.com>

---
+++ distributedhardware/devicemanagerjstest/src/main/js/default/pages/index/index.js
@@ -0,0 +1,31 @@
+import file from '@system.file'
+import app from '@system.app'
+
+import {Core} from 'deccjsunit/index'
+
+export default {
+    data: {
+        title: ""
+    },
+    onInit() {
+        this.title = this.$t('strings.world');
+    },
+    onShow() {
+        console.info('onShow finish')
+        const core = Core.getInstance()
+        /* const expectExtend = new ExpectExtend({
+            'id': 'extend'
+        })*/
+        /*const reportExtend = new ReportExtend(file)
+        core.addService('expect', expectExtend)
+        core.addService('report', reportExtend)*/
+        core.init()
+
+
+        const configService = core.getDefaultService('config')
+        configService.setConfig(this)
+
+        require('../../../test/List.test')
+        core.execute()
+    }
+}
