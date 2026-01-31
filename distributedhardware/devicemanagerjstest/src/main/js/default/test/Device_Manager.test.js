760c6918edae0ef1e67e999766ac4bb66f231d4b
torish14k <uanganxinyiw829@163.com>
2026-01-31 19:15:12 +0800

    edit warning
    Signed-off-by: liuxiaopei <liuxiaopei@huawei.com>

--- distributedhardware/devicemanagerjstest/src/main/js/default/test/Device_Manager.test.js
+++ distributedhardware/devicemanagerjstest/src/main/js/default/test/Device_Manager.test.js
@@ -1,10 +1,22 @@
+/*
+ * Copyright (C) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import app from '@system.app';
 import {describe, beforeAll, beforeEach, afterEach, afterAll, it, expect} from 'deccjsunit/index'
 import deviceManager from '@ohos.distributedHardware.deviceManager'
 
-
 describe('deviceManagerTest', function() {
-    console.log("---deviceManager接口测试---")
 
     /*
      * @tc.number  deviceManagerTest_createDeviceManager_0100
@@ -288,7 +300,6 @@ describe('deviceManagerTest', function() {
         done();
     })
 
- 
     /*
      * @tc.number  deviceManagerTest_on-deviceStateChange_0100
      * @tc.name    testcase for deviceStateChange on
@@ -448,6 +459,4 @@ describe('deviceManagerTest', function() {
         console.log("----------stop running deviceManagerTest_off-serviceDie_0100----------");
         done();
     })
-
-
 })
